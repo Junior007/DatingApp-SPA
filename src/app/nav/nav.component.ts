99553e83572a20c1c4cd17bba478dd3b14d51163
Junior007 <agilgome@gmail.com>
2019-04-25 21:13:18 +0200

    primer servicio

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { debug } from 'util';
+import { AuthService } from '../_services/auth.service';
 
 @Component({
   selector: 'app-nav',
@@ -9,12 +10,27 @@ import { debug } from 'util';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
   }
 
-  login(){
-    console.log(this.model);
+  login() {
+    this.authService.login(this.model).subscribe(
+      next => {
+        console.log('login realizado');
+      },
+      error => {
+        console.log('algo ha pasado');
+      });
+  }
+
+  loggedIn(){
+    const token = localStorage.getItem('token');
+    return !!token;
+  }
+
+  logOut(){
+    localStorage.removeItem('token');
   }
 }

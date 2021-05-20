8edfe3f4de8ce97c72694e363b95e0305e1d3120
Junior007 <agilgomez@gmail.com>
2021-05-20 08:11:02 +0200

    forms

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { AuthService } from '../../_services/auth/auth.service';
 import { LoginUser } from 'src/app/models/all';
 import { AlertifyService } from 'src/app/_services/alertify/alertify.service';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -10,27 +11,61 @@ import { AlertifyService } from 'src/app/_services/alertify/alertify.service';
 })
 export class RegisterComponent implements OnInit {
 
-  model: LoginUser = {username: '', password: ''};
+  model: LoginUser = { username: '', password: '' };
+  registerForm: FormGroup;
   @Output() cancelRegister = new EventEmitter();
 
-  constructor(private authService: AuthService, private alertifyService:AlertifyService) { }
+
+  constructor(private authService: AuthService
+            , private alertifyService: AlertifyService
+            , private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.createRegisterForm();
   }
   //
-  register(){
-    this.authService.register(this.model)
-    .subscribe(
-      () => {
-        this.alertifyService.message('Registro completo');
-      }
-      , error => {
-        this.alertifyService.warning(error);
+  createRegisterForm() {
+    this.registerForm = this.fb.group({
+      gender: ['male', Validators.required],
+      username: ['', Validators.required],
+      knownAs: ['', Validators.required],
+      dateOfBirth: ['', Validators.required],
+      city: ['', Validators.required],
+      country: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]],
+      confirmPassword: ['', Validators.required]
 
-      });
+    }, { validator: this.passwordMatchValidator });
+    /*
+    this.registerForm = new FormGroup(
+      {
+        username: new FormControl('', Validators.required),
+        password: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]),
+        confirmPassword: new FormControl('', Validators.required)
+      }, this.passwordMatchValidator
+    );
+    */
   }
   //
-  cancel(){
+  register() {
+    /*this.authService.register(this.model)
+      .subscribe(
+        () => {
+          this.alertifyService.message('Registro completo');
+        }
+        , error => {
+          this.alertifyService.warning(error);
+
+        });*/
+    console.log(this.registerForm.value);
+  }
+  //
+  passwordMatchValidator(g: FormGroup) {
+    return g.get('password').value === g.get('confirmPassword').value ? null : { mismatch: true };
+
+  }
+  //
+  cancel() {
     this.cancelRegister.emit(false);
     console.log('cancelled');
   }
